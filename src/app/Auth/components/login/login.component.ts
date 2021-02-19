a6e5ed354ab074ed8e539dd9e8ff3c1d406a0926
Sepandar.d <sepandar.derakhshandeh@gmail.com>
2021-02-19 18:20:30 +0330

    step 1 for  login  component

--- src/app/Auth/components/login/login.component.ts
+++ src/app/Auth/components/login/login.component.ts
@@ -1,15 +1,65 @@
+import { Placeholder } from '@angular/compiler/src/i18n/i18n_ast';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { FormlyFieldConfig } from '@ngx-formly/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  profileForm = new FormGroup({
+    value: new FormControl(''),
+  });
+  redemptionOfYearsFormConfig: FormlyFieldConfig[];
+  redemptionOfYearsForm = new FormGroup({});
+  redemptionOfYearsFormModel: any = {};
+  onSubmit() {
+    console.log(this.profileForm.value);
+  }
+  constructor() {}
 
   ngOnInit(): void {
+    this.redemptionOfYearsinitForm();
   }
-
+  redemptionOfYearsinitForm() {
+    this.redemptionOfYearsFormConfig = [
+      {
+        fieldGroupClassName: 'flex-container',
+        fieldGroup: [
+          {
+            className: 'flex-25 padding-10',
+            key: 'EstablishmentAmount',
+            type: 'input',
+            templateOptions: {
+              label: 'Email',
+              Placeholder: 'Example@',
+              required: true,
+            },
+          },
+          {
+            className: 'flex-25 padding-10',
+            key: 'EstablishmentAmount',
+            type: 'input',
+            templateOptions: {
+              label: 'Email',
+              Placeholder: 'Example@',
+              required: true,
+            },
+          },
+          {
+            className: 'flex-25 padding-10',
+            key: 'EstablishmentAmount',
+            type: 'input',
+            templateOptions: {
+              label: 'Email',
+              Placeholder: 'Example@',
+              required: true,
+            },
+          },
+        ],
+      },
+    ];
+  }
 }
