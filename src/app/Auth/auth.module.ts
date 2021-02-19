a549df19c1729c849bb02a969a62e53e4c6d14a2
Sepandar.d <sepandar.derakhshandeh@gmail.com>
2021-02-19 18:18:13 +0330

    install formly Material

--- src/app/Auth/auth.module.ts
+++ src/app/Auth/auth.module.ts
@@ -5,9 +5,23 @@ import { SignupComponent } from './components/signup/signup.component';
 import { AuthRoutingModule } from './auth.routing';
 import { AuthComponent } from './components/auth/auth.component';
 import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyMaterialModule } from '@ngx-formly/material';
 
 @NgModule({
   declarations: [LoginComponent, SignupComponent, AuthComponent],
-  imports: [CommonModule, AuthRoutingModule, RouterModule],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    RouterModule,
+    MatCardModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormlyModule.forRoot(),
+    FormlyMaterialModule,
+  ],
 })
 export class AuthModule {}
