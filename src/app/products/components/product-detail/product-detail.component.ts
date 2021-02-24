5a9acae162cc97befb1ba2dbe27d5d404c8bf71d
Sepandar.d <sepandar.derakhshandeh@gmail.com>
2021-02-25 02:59:44 +0330

    get id from url and compare with other products

---
+++ src/app/products/components/product-detail/product-detail.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/models/products';
+import { ProductsService } from '../../shared/products.service';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css'],
+})
+export class ProductDetailComponent implements OnInit {
+  url: String = '';
+  selectedProduct: Product;
+  constructor(private productSrv: ProductsService) {}
+
+  ngOnInit(): void {
+    let productIdArr: any[] = [];
+
+    this.url = window.location.href;
+    productIdArr = this.url.split('=');
+
+    this.productSrv.fakeData.forEach((element) => {
+      if (element.prodcutId == productIdArr[1]) {
+        this.selectedProduct = element;
+      }
+    });
+  }
+}
