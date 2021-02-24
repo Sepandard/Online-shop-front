605e4ac9353eaea2811077096cca999fa6b51ab5
Sepandar.d <sepandar.derakhshandeh@gmail.com>
2021-02-25 02:54:18 +0330

    send product id by url

--- src/app/products/components/product-card/product-card.component.ts
+++ src/app/products/components/product-card/product-card.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductsService } from '../../shared/products.service';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.css']
+  styleUrls: ['./product-card.component.css'],
 })
 export class ProductCardComponent implements OnInit {
+  product: any[] = this.productSrv.fakeData;
 
-  constructor() { }
+  constructor(private productSrv: ProductsService, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onClinkToDetaliPage(item) {
+    this.router.navigate([
+      '/online-shop/products/product-detail',
+      { Id: item.prodcutId },
+    ]);
   }
-
 }
