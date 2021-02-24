6220eb99a9bef2f9777cacc177b8b3ec103c4416
Sepandar.d <sepandar.derakhshandeh@gmail.com>
2021-02-25 02:56:25 +0330

    create fake data for test

--- src/app/products/shared/products.service.ts
+++ src/app/products/shared/products.service.ts
@@ -1,9 +1,83 @@
 import { Injectable } from '@angular/core';
+import { Product } from 'src/models/products';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
-
-  constructor() { }
+  constructor() {}
+  fakeData = [
+    {
+      prodcutId: 1,
+      productName: 'ssss',
+      descriptionProduct: 'sasadasd',
+      ProductImageUrl:
+        'https://material.angular.io/assets/img/examples/shiba1.jpg',
+      priceProduct: 2000,
+      categoryProduct: 'asdasd',
+    },
+    {
+      prodcutId: 1,
+      productName: 'ssss',
+      descriptionProduct: 'sasadasd',
+      ProductImageUrl:
+        'https://material.angular.io/assets/img/examples/shiba1.jpg',
+      priceProduct: 2000,
+      categoryProduct: 'asdasd',
+    },
+    {
+      prodcutId: 1,
+      productName: 'ssss',
+      descriptionProduct: 'sasadasd',
+      ProductImageUrl:
+        'https://material.angular.io/assets/img/examples/shiba1.jpg',
+      priceProduct: 2000,
+      categoryProduct: 'asdasd',
+    },
+    {
+      prodcutId: 1,
+      productName: 'ssss',
+      descriptionProduct: 'sasadasd',
+      ProductImageUrl:
+        'https://material.angular.io/assets/img/examples/shiba1.jpg',
+      priceProduct: 2000,
+      categoryProduct: 'asdasd',
+    },
+    {
+      prodcutId: 1,
+      productName: 'ssss',
+      descriptionProduct: 'sasadasd',
+      ProductImageUrl:
+        'https://material.angular.io/assets/img/examples/shiba1.jpg',
+      priceProduct: 2000,
+      categoryProduct: 'asdasd',
+    },
+    {
+      prodcutId: 1,
+      productName: 'ssss',
+      descriptionProduct: 'sasadasd',
+      ProductImageUrl:
+        'https://material.angular.io/assets/img/examples/shiba1.jpg',
+      priceProduct: 2000,
+      categoryProduct: 'asdasd',
+    },
+    {
+      prodcutId: 1,
+      productName: 'ssss',
+      descriptionProduct: 'sasadasd',
+      ProductImageUrl:
+        'https://material.angular.io/assets/img/examples/shiba1.jpg',
+      priceProduct: 2000,
+      categoryProduct: 'asdasd',
+    },
+    {
+      prodcutId: 1,
+      productName: 'ssss',
+      descriptionProduct: 'sasadasd',
+      ProductImageUrl:
+        'https://material.angular.io/assets/img/examples/shiba1.jpg',
+      priceProduct: 2000,
+      categoryProduct: 'asdasd',
+    },
+  ];
 }
