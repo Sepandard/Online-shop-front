f09078bf763c0941c88471fbd2d53788dc176b5e
Sepandar.d <sepandar.derakhshandeh@gmail.com>
2021-03-16 13:25:33 +0330

    add product search and  getcategory maping them

--- src/app/products/shared/products.service.ts
+++ src/app/products/shared/products.service.ts
@@ -1,11 +1,43 @@
 import { Injectable } from '@angular/core';
-import { Product } from 'src/models/products';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { BaseService } from 'shared/base-service.service';
+import { CategoryMenu } from 'src/models/category-menu';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
-  constructor() {}
+  CategoryItem: CategoryMenu;
+  constructor(private baseSrv: BaseService) {}
+  CategoryMenu$: BehaviorSubject<any[]> = new BehaviorSubject<any[]>([]);
+  public get CategoryMenu(): Observable<any[]> {
+    return this.CategoryMenu$.asObservable();
+  }
+  getcategory(model?: String) {
+    return this.baseSrv.getReq('product/searchProductCategory', model);
+  }
+
+  getCategoryMenu() {
+    let CategoryArr = [];
+    this.getcategory().subscribe((res: any) => {
+      if (res.data[0].ProductCategory) {
+        res.data[0].ProductCategory.forEach((element) => {
+          let item = new CategoryMenu(element);
+          CategoryArr.push(item);
+        });
+        console.log(CategoryArr);
+        this.CategoryMenu$.next(CategoryArr);
+      }
+    });
+    return CategoryArr;
+  }
+  getProducts(productname?){
+    this.baseSrv.getReq('product/searchProduct',productname).subscribe((res)=>{
+      console.log(res);
+      
+    })
+  }
+
   fakeData = [
     {
       prodcutId: 1,
