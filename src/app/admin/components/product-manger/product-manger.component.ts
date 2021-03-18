68bfa7c129fbbef748d1d0a02b9eb98ba90ae68f
Sepandar.d <sepandar.derakhshandeh@gmail.com>
2021-03-18 11:42:23 +0330

    create ProductManger Component

---
+++ src/app/admin/components/product-manger/product-manger.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-manger',
+  templateUrl: './product-manger.component.html',
+  styleUrls: ['./product-manger.component.css']
+})
+export class ProductMangerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
