d41ff2e779a5f4bd842e6bd8a607cef96711a6b5
Sepandar.d <sepandar.derakhshandeh@gmail.com>
2021-03-26 19:53:53 +0430

    create order class

---
+++ src/models/order.ts
@@ -0,0 +1,17 @@
+export class Order {
+    // order_id: number;
+    // order_price?: string;
+    // productname: String;
+    // productdescription: String;
+    // productprice: String;
+    // productcategoryid?: string;
+  
+    // constructor(productModel?) {
+    //   this.productid = productModel.productid
+    //   this.productimage = productModel.productimage
+    //   this.productname = productModel.productname
+    //   this.productdescription = productModel.productdescription
+    //   this.productprice = productModel.productprice;
+    //   this.productcategoryid = productModel.productcategoryid
+    // }
+  }
