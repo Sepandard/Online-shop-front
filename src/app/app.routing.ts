ef9384e529e6a435a767ca31a6063ee2e5263a2d
Sepandar.d <sepandar.derakhshandeh@gmail.com>
2021-05-17 15:51:18 +0430

    [FIX] fix bug in routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,11 +3,16 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 const routes: Routes = [
   {
     path: 'online-shop',
     loadChildren: () =>
       import('./layout/layout.module').then((m) => m.LayoutModule),
+  },
+  {
+    path: '**',
+    redirectTo: '/online-shop/products/product-list',
   },
 ];
 
