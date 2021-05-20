ec86b55a1ce3c7ad25d00f876d0c8c17e2856357
Sepandar.d <sepandar.derakhshandeh@gmail.com>
2021-05-21 01:45:39 +0430

    [FEAT] add new product property

--- src/models/products.ts
+++ src/models/products.ts
@@ -1,17 +1,21 @@
 export class Product {
   productid: number;
-  productimage?: string;
+  productimageid?: string;
   productname: String;
   productdescription: String;
   productprice: String;
   productcategoryid?: string;
+  image_name?:string
+  categoryproductname?:string
 
   constructor(productModel?) {
     this.productid = productModel.productid
-    this.productimage = productModel.productimage
+    this.productimageid = productModel.productimageid
     this.productname = productModel.productname
     this.productdescription = productModel.productdescription
     this.productprice = productModel.productprice;
     this.productcategoryid = productModel.productcategoryid
+    this.image_name = productModel.image_name
+    this.categoryproductname = productModel.categoryproductname
   }
 }
